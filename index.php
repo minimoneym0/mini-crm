70490080da01cd0eb99ed057323156417e261f50
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-12 13:45:00 +0300

    add display errors and require Database.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<? phpinfo();?>
+<?php
+// вывод всех ошибок на время отладки
+error_reporting(E_ALL); 
+ini_set('display_errors', 1);
+
+// подключаем работу с бд
+require_once 'app/models/Database.php';
