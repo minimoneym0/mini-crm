0514094fe4f5ed6033ec8a9d432321fa961bbb03
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-13 13:14:12 +0300

    add require controllers

--- index.php
+++ index.php
@@ -3,5 +3,9 @@
 error_reporting(E_ALL); 
 ini_set('display_errors', 1);
 
-// подключаем работу с бд
+// подключаем работу с бд и модель User
 require_once 'app/models/Database.php';
+require_once 'app/models/User.php';
+// подключаем контроллеры
+require_once 'app/controllers/users/UsersController.php';
+require_once 'app/controllers/users/AuthController.php';
