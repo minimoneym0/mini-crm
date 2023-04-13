f625c5b8e03d121a0f428bfe092dde46a3c2d8bd
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-13 15:20:03 +0300

    coding class User, connect DB and get data for array users

--- app/models/User.php
+++ app/models/User.php
@@ -1,7 +1,22 @@
 <?php
 // модель получает данные, применяет нужный метод и пишет в базу
 class User{
+    private $db;
+    // конструктором подключаемся к БД, через созданные ранее ф-ии в классе Database
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+    // метод, который забирает из БД пользователей и запаковыает их в массив
     public function readAll(){
-       
+       $result = $this->db->query("SELECT * FROM users"); // запрос к бд
+
+       $users = [];
+       // преобразуем данные в массив построчно через fetch_assoc()
+       while($row=$result->fetch_assoc()){
+            $users = $row;
+       }
+
+       return $users;
     }
 }
