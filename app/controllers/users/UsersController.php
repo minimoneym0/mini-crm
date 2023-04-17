55889eb036090ea1382f3e03711f0722a396924e
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-17 15:24:43 +0300

    add code functions create and store

--- app/controllers/users/UsersController.php
+++ app/controllers/users/UsersController.php
@@ -8,4 +8,25 @@ class UsersController{
 
         include 'app/views/users/index.php'; // подключаем файл, который будет html шаблоном для списка пользователей
     }
+    // пишем метод, который вызывает шаблон страницы
+    public function create(){
+        include 'app/views/users/create.php';
+    }
+
+    public function store(){
+        if(isset($_POST['login']) && isset($_POST['password']) && isset($_POST['confirm_password']) && isset($_POST['is_admin'])){
+            $password = $_POST['password'];
+            $confirm_password = $_POST['confirm_password'];
+            // если пароли не сопадают
+            if($password !== $confirm_password){
+                echo "Password does not match";
+                return;
+            }
+            // если ошибок нет, вызываем модель User
+            $userModel = new User();
+            $userModel->create($_POST); // передаем в ф-ю глоб массив с данными из формы
+        }
+        header("Location: ?page=users");
+    }
+
 }
