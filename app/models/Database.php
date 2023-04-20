7f70f4b59204828f49ec828f5b9221ab0ecd2d4a
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-20 10:30:25 +0300

    rewrite method for include DB

--- app/models/Database.php
+++ app/models/Database.php
@@ -5,22 +5,31 @@ class Database{
     private static $instance = null;
 
     private $conn;
-    private $host = 'localhost';
-    private $user = 'root';
-    private $pass = 'root';
-    private $name_db = 'minicrm';
 
     // создаем конструктор с подключением к бд
     private function __construct()
-    {   // объект подключения к бд
-        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->name_db); // указываем параметры подключения к бд, подключаемся
-        if($this->conn->connect_error) die('connect failed'.$this->conn->connect_error); // проверяем коннект к бд
+    {   // объект подключения к бд через PDO
+        $config = require_once __DIR__ . '/../../config.php'; // подключаемся к конфигу
+        $db_host = $config['db_host'];
+        $db_user = $config['db_user'];
+        $db_pass = $config['db_pass'];
+        $db_name_db = $config['db_name_db'];
+
+        try{
+            // пишем коннектор, будем получать хост и имя БД
+            $dsn = "mysql:host=$db_host;dbname=$db_name_db";
+            $this->conn = new PDO($dsn, $db_user, $db_pass); // создаем экземпляр PDO и передаем данные
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // передаем считывание ошибок и исключений
+        }catch(PDOException $e){
+            // если подключение не сработает, выведет ошибки
+            echo "Connect failed: ".$e->getMessage(); 
+        }
     }
 
     // создаем метод, возвращающий объект класса Database
     public static function getInstance(){
-        if(!self::$instance){
-            self::$instance = new Database();
+        if(!isset(self::$instance)){
+            self::$instance = new self();
         }
         return self::$instance;
     }
