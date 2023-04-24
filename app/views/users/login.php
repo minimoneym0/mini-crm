9c3650be197290a1afe64adba8f155a174248e7c
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-24 15:09:19 +0300

    add html for form login and register

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,1 +1,28 @@
 <?php
+
+$title = 'Authorization';
+ob_start();?>
+
+<h1>Authorization</h1>
+<form method="post" action="?page=auth&action=authenticate"> <!-- указываем action=store, ссылаясь на метод store в UserController -->
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="remember" name="remember">
+        <lable class="form-check-label" for="remember">Remember me</lable>
+    </div>
+    <button class="btn btn-primary" type="submit">Login</button>
+</form>
+<div class="mt-4">
+    <p>Don't have an account? <a href="index.php?page=login">Register here</a></p>
+</div>
+
+<?php $content = ob_get_clean();
+
+include 'app/views/layout.php'; // сюда будет передаваться контент размещенный выше между ob_start(); и $content = ob_get_clean();
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,2 +1,32 @@
 <?php
 
+$title = 'Register User';
+ob_start();?>
+
+<h1>Register User</h1>
+<form method="post" action="?page=auth&action=store"> <!-- указываем action=store, ссылаясь на метод store в UserController -->
+    <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" class="form-control" id="username" name="username" required>
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <div class="form-group">
+        <label for="confirm_password">Confirm Password</label>
+        <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+    </div>
+    <button class="btn btn-primary" type="submit">Register</button>
+</form>
+<div class="mt-4">
+    <p>Already have an account? <a href="index.php?page=login">Login here</a></p>
+</div>
+
+<?php $content = ob_get_clean();
+
+include 'app/views/layout.php'; // сюда будет передаваться контент размещенный выше между ob_start(); и $content = ob_get_clean();
