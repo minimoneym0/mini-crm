caef7e82ca005f7565cbe14c96eaad490589cffa
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-25 14:06:35 +0300

    write functions getAllRoles, createRole, getRoleById, updateRole

---
+++ app/models/roles/Role.php
@@ -0,0 +1,79 @@
+<?php
+// модель получает данные, применяет нужный метод и пишет в базу
+class Role{
+    private $db;
+    // конструктором подключаемся к БД, через созданные ранее ф-ии в классе Database
+    // затем смотрим, если таблица с пользователями уже есть, то ничего не делаем, если нет, то отправляем запрос на создание таблицы users
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+        try{
+            $result = $this->db->query("SELECT 1 FROM `roles` LIMIT 1");
+        }catch(PDOException $e){
+            $this->createDbTable();
+        }
+    }
+
+    public function createDbTable(){
+        $roleTableQuery = "CREATE TABLE IF NOT EXISTS `roles`(
+            `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `role_name` VARCHAR(255) NOT NULL,
+            `role_description` TEXT
+        )";
+
+            try{
+                $this->db->exec($roleTableQuery);
+                return true;
+            }catch(PDOException $e){
+                return false;
+            }
+    }
+// пишем метод для получения всех ролей
+    public function getAllRoles(){
+        $query = "SELECT * FROM roles";
+        try{
+            $stmt = $this->db->prepare($query); // подготовл запрос
+            $roles = $stmt->fetchAll(PDO::FETCH_ASSOC); // вывод в переменную 
+            return $roles;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+// метод в котором создаем роль
+    public function createRole($role_name, $role_description){
+
+        $query = "INSERT INTO roles (role_name, role_description) VALUES (?,?)";
+
+        try{
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$role_name, $role_description]);
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+// метод для получения конкретной роли по id
+    public function getRoleById($id){
+        $query = 'SELECT * FROM roles WHERE id = ?';
+        try{
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$id]);
+            $role = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $role ? $role : false;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+
+    public function updateRole($id, $role_name, $role_description){
+        $query = "UPDATE roles SET role_name = ?, role_description = ? WHERE id = ?";
+        try{
+            $stmt = $this->db->prepare($query);
+            $stmt->execute([$role_name, $role_description, $id]);
+
+            return true;
+        }catch(PDOException $e){
+            return false;
+        }
+    }
+}
