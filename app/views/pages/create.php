c9f8442abc2688b9690f38ffba63e82fac4441db
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-26 13:04:33 +0300

    add code for create.php(views/pages)

---
+++ app/views/pages/create.php
@@ -0,0 +1,21 @@
+<?php
+
+$title = 'Create Page';
+ob_start();?>
+
+<h1>Create Page</h1>
+<form method="post" action="?page=pages&action=store"> <!-- указываем action=store, ссылаясь на метод store в UserController -->
+    <div class="form-group">
+        <label for="title">Page title</label>
+        <input type="text" class="form-control" id="title" name="title" required>
+    </div>
+    <div class="form-group">
+        <label for="slug">Page slug</label>
+        <input type="text" class="form-control" id="slug" name="slug" required>
+    </div>
+    <button class="btn btn-primary" type="submit">Create Page</button>
+</form>
+
+<?php $content = ob_get_clean();
+
+include 'app/views/layout.php'; // сюда будет передаваться контент размещенный выше между ob_start(); и $content = ob_get_clean();
