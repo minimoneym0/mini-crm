c7fd74229cfdfb870dfcda7cf8bc3fa901ccc2c4
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-04-27 11:26:42 +0300

    use spl_autoload_register in autoload

---
+++ autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+spl_autoload_register(function ($class){
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $classPath = __DIR__ . DIRECTORY_SEPARATOR . $class . 'php'; // собираем путь к файлу класса
+
+    // пишем проверку существования файла в директории
+    if(file_exists($classPath)){ // проверяем наличие файла
+        require_once $classPath; //  и подключаем его
+    }else{
+        $class = str_replace('controllers', 'app/controllers', $class);
+        $classPath = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+
+        if(file_exists($classPath)){
+            require_once $classPath;
+        }else{
+            throw new Exception("Class {$class} not found in {$classPath}");
+        }
+    }
+
+}
+);
