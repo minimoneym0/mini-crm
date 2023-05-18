95db731f583c207c9596f70ba92c0c9c2f38d6cb
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-05-18 09:11:20 +0300

    add route for TaskController in Router

--- app/router.php
+++ app/router.php
@@ -16,6 +16,7 @@ class Router{
         '/^\/'.APP_BASE_PATH.'\/pages(\/(?P<action>[a-z]+)(\/(?P<id>\d+))?)?$/' => ['controller'=>'pages\\PageController'],
         '/^\/' . APP_BASE_PATH . '\/auth(\/(?P<action>[a-z]+)(\/(?P<id>\d+))?)?$/' => ['controller' => 'auth\\AuthController'],
         '/^\/' . APP_BASE_PATH . '\/todo\/category(\/(?P<action>[a-z]+)(\/(?P<id>\d+))?)?$/' => ['controller' => 'todo\\category\\CategoryController'],
+        '/^\/' . APP_BASE_PATH . '\/todo\/tasks(\/(?P<action>[a-z]+)(\/(?P<id>\d+))?)?$/' => ['controller' => 'todo\\tasks\\TaskController'],
     ];
 
 
