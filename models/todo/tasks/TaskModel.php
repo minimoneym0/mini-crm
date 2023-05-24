43fbe67ce503fd10a570ea4c43265676c2e4bfa0
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-05-24 13:21:46 +0300

    edit function getAllTasksByIdUser SQL query for work only open tasks

--- models/todo/tasks/TaskModel.php
+++ models/todo/tasks/TaskModel.php
@@ -61,7 +61,7 @@ class TaskModel {
     public function getAllTasksByIdUser($user_id){
 
         try{
-            $stmt = $this->db->prepare("SELECT * FROM todo_list WHERE user_id = :user_id");
+            $stmt = $this->db->prepare("SELECT * FROM todo_list WHERE finish_date > NOW() AND user_id = :user_id AND status != 'completed' ORDER BY ABS(TIMESTAMPDIFF(SECOND, NOW(), finish_date))");
             $stmt->bindParam(':user_id', $user_id, \PDO::PARAM_INT);
             $stmt->execute();
             $todo_list = [];
