04748acbc079459c79e64032a284430cc4a8e585
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-05-30 13:44:08 +0300

    написали скрипт для обработки действующих задач и отрисовки календаря с этими задачами

--- app/views/index.php
+++ app/views/index.php
@@ -13,41 +13,45 @@ ob_start();?>
 
 <?php $path = '/'. APP_BASE_PATH . '/todo/tasks/task/';?>
 <!-- пишем скрипт для вывода календаря и его работы -->
-    <script>
-       // получение данных о задачах из нашего php контроллера 
-        const tasksJson = <?= json_encode($tasksJson);?>
-        const tasks = JSON.parse(tasksJson); // парсим содержимое в массив объектов
-// преобразование данных(массива) в задачи для календаря
-        const events = tasks.map((task) => {
-            return {
-                title: task.title,
-                start: new Date(task.created_at),
-                end: new Date(task.finish_date),
-                extendProps: {
-                    task_id: task.id,    
-                },
-            };
-        });
-// обработчки событий загрузки нашего DOM объекта
-        document.addEventListener("DOMContentLoaded", function(){
-            const calendarE1 = document.getElementById('calendar');
-            // инициализация календаря с настройками
-            const calendar = new FullCalendar.Calendar(calendarE1, {
-                initialView: 'dayGridMonth',
-                event: events, // задачи в виде событий на календаре
-                eventClick : function(info){
-                    const taskId = info.event.extendProps.taskId;
-                    // url куда переводить пользователя после клика(стр конкретной задачи)
-                    const taskUrl = '<?= $path;?>${taskId}';
-                    // переход на страницу задачи
-                    window.location.href = taskUrl;
+<script>
+// Получение данных о задачах, из нашего PHP-контроллера
+const tasksJson = <?= json_encode($tasksJson) ?>;
+const tasks = JSON.parse(tasksJson); // tasks это массив объектов
+// Преобразование данных (массива) в задачи для календаря
+const events = tasks.map((task) => {
+    return {
+      title: task.title,
+      start: new Date(task.created_at), // Используйте created_at вместо start_date
+      end: new Date(task.finish_date), // Используйте finish_date вместо end_date
+      extendedProps: {
+        task_id: task.id, // добавьте ID задачи в расширенные свойства
+        },
+    };
+  });
 
-                },
-            });
-            // запускаем рендер календаря
-            calendar.render();
-        });
-    </script>
+// Обработчик событий загрузки DOM
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar');
+
+    // Инициализация календаря с настройками
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+         initialView: 'dayGridMonth',
+        
+        events: events, // Задачи в виде событий на календаре
+        eventClick: function (info) {
+            const taskId = info.event.extendedProps.task_id;
+
+            // URL для  адреса страницы конкретной задачи
+            const taskUrl = `<?=$path;?>${taskId}`;
+
+            //переход на страницу задачи
+            window.location.href = taskUrl;
+        },
+    });
+
+    calendar.render();
+});
+</script>
 
 <?php $content = ob_get_clean();
 
--- controllers/home/HomeController.php
+++ controllers/home/HomeController.php
@@ -6,7 +6,12 @@ use models\todo\tasks\TaskModel;
 class HomeController{
 
     public function index(){
-        $userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 0;
+        $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 0;
+
+        $taskModel = new TaskModel();
+        $tasks = $taskModel->getAllTasksByIdUser($user_id); // получаем задачи
+
+        $tasksJson = json_encode($tasks); // парсим задачи из нашего массива в json объект
 
         require_once 'app/views/index.php';
     }
