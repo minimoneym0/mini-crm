9b58b0ab3d82bb79b2407689eca7871378eafdb5
minimoneym0 <kuznetsoff.site@yandex.ru>
2023-05-30 13:20:49 +0300

    add script for calendar in views/index.php

--- app/views/index.php
+++ app/views/index.php
@@ -1,6 +1,6 @@
 <?php
 // редирект с index.php на /
-if($_SERVER['REQUEST_URI'] == '/minicrm/index.php'){
+if($_SERVER['REQUEST_URI'] == APP_BASE_PATH.'/index.php'){
     header('Location: /minicrm');
 }
 
@@ -8,6 +8,46 @@ $title = 'Home page';
 ob_start();?>
 
 <h1>Home page</h1>
+<!-- размещаем див с календарем -->
+<div id='calendar'></div>
+
+<?php $path = '/'. APP_BASE_PATH . '/todo/tasks/task/';?>
+<!-- пишем скрипт для вывода календаря и его работы -->
+    <script>
+       // получение данных о задачах из нашего php контроллера 
+        const tasksJson = <?= json_encode($tasksJson);?>
+        const tasks = JSON.parse(tasksJson); // парсим содержимое в массив объектов
+// преобразование данных(массива) в задачи для календаря
+        const events = tasks.map((task) => {
+            return {
+                title: task.title,
+                start: new Date(task.created_at),
+                end: new Date(task.finish_date),
+                extendProps: {
+                    task_id: task.id,    
+                },
+            };
+        });
+// обработчки событий загрузки нашего DOM объекта
+        document.addEventListener("DOMContentLoaded", function(){
+            const calendarE1 = document.getElementById('calendar');
+            // инициализация календаря с настройками
+            const calendar = new FullCalendar.Calendar(calendarE1, {
+                initialView: 'dayGridMonth',
+                event: events, // задачи в виде событий на календаре
+                eventClick : function(info){
+                    const taskId = info.event.extendProps.taskId;
+                    // url куда переводить пользователя после клика(стр конкретной задачи)
+                    const taskUrl = '<?= $path;?>${taskId}';
+                    // переход на страницу задачи
+                    window.location.href = taskUrl;
+
+                },
+            });
+            // запускаем рендер календаря
+            calendar.render();
+        });
+    </script>
 
 <?php $content = ob_get_clean();
 
--- controllers/home/HomeController.php
+++ controllers/home/HomeController.php
@@ -1,9 +1,13 @@
 <?php
 namespace controllers\home;
 
+use models\todo\tasks\TaskModel;
+
 class HomeController{
 
     public function index(){
+        $userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 0;
+
         require_once 'app/views/index.php';
     }
 }
